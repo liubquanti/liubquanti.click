e937b55f20188dd3f6f914f103dc5a8c14307556
Oleh Liubchenko <doroleijn@gmail.com>
2023-07-15 12:09:07 +0300

    у

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function toggleAudio() {
     var audio = document.getElementById("audio");
+    var photo = document.querySelector(".item-meme");
+
     if (audio.paused) {
         audio.play();
+        photo.classList.add("playing");
     } else {
         audio.pause();
+        photo.classList.remove("playing");
     }
 }
