4852e50e2a2c6e27acb44c395bea08c67c8c8071
Oleh Liubchenko <doroleijn@gmail.com>
2023-07-15 22:26:22 +0300

    done gear

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function toggleAudio() {
     var audio = document.getElementById("audio");
     var photo = document.querySelector(".item-meme");
+    var body = document.querySelector("body");
 
     if (audio.paused) {
         audio.play();
         photo.classList.add("playing");
+        body.classList.add("pulse");
     } else {
         audio.pause();
         photo.classList.remove("playing");
+        body.classList.remove("pulse");
     }
 }
 
@@ -15,4 +18,7 @@ function resetPhoto() {
     var photo = document.querySelector(".item-meme");
     photo.classList.remove("playing");
     photo.style.transform = "rotate(0deg)";
+
+    var body = document.querySelector("body");
+    body.classList.remove("pulse");
 }
