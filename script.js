01c032cc594a4460b0538a1613d683acb8a81f28
Oleh Liubchenko <doroleijn@gmail.com>
2023-08-24 14:45:02 +0200

    fix

--- script.js
+++ script.js
@@ -23,11 +23,18 @@ function resetPhoto() {
     body.classList.remove("pulse");
 }
 
-// script.js
 window.addEventListener('load', function() {
     var currentDate = new Date();
+    
     var birthDate = new Date("2005-10-27");
-    var timeDiff = currentDate - birthDate;
-    var age = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365.25));
+    
+    var age = currentDate.getFullYear() - birthDate.getFullYear();
+
+    var birthDateThisYear = new Date(currentDate.getFullYear(), birthDate.getMonth(), birthDate.getDate());
+
+    if (currentDate < birthDateThisYear) {
+        age--;
+    }
+
     document.getElementById("age").textContent = age;
 });
