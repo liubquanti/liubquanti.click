afb9f3bcc553780c044b11a11c2c7db4b4889038
Oleh Liubchenko <doroleijn@gmail.com>
2023-08-24 14:18:06 +0200

    years

--- script.js
+++ script.js
@@ -22,3 +22,12 @@ function resetPhoto() {
     var body = document.querySelector("body");
     body.classList.remove("pulse");
 }
+
+// script.js
+window.addEventListener('load', function() {
+    var currentDate = new Date();
+    var birthDate = new Date("2005-10-27");
+    var timeDiff = currentDate - birthDate;
+    var age = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365.25));
+    document.getElementById("age").textContent = age;
+});
