89e4d2fa3750a6f9acf0fdac1771f60dbdbfca1f
Oleh Liubchenko <doroleijn@gmail.com>
2024-05-13 20:54:18 +0200

    A lot of changes

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-function toggleAudio() {
-    var audio = document.getElementById("audio");
-    var photo = document.querySelector(".item-meme");
-    var body = document.querySelector("body");
-
-    if (audio.paused) {
-        audio.play();
-        photo.classList.add("playing");
-        body.classList.add("pulse");
-    } else {
-        audio.pause();
-        photo.classList.remove("playing");
-        body.classList.remove("pulse");
-    }
-}
-
-function resetPhoto() {
-    var photo = document.querySelector(".item-meme");
-    photo.classList.remove("playing");
-    photo.style.transform = "rotate(0deg)";
-
-    var body = document.querySelector("body");
-    body.classList.remove("pulse");
-}
-
 window.addEventListener('load', function () {
     var currentDate = new Date();
 
